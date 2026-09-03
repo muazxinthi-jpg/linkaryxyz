b4f95b05cd40baa06b4b5dd2fb4c60975a296a3d
Muaz Linkary <muazxinthi@gmail.com>
2026-09-03 23:04:17 +0200

    Load Partner Discovery stabilization styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import './profile-next.css';
 import './growth.css';
 import './partners.css';
 import './ux-system.css';
+import './partner-discovery-stabilization.css';
 
 const cdpConfig: Config = {
   projectId: 'ec85aa2b-208c-4ec9-a0f2-3da31a8e2218',
